f254899e4d9cb30d2da7e63c2fc59da4c083b5e3
Juan <juan@Juan.consulbank>
2025-05-19 18:00:13 -0400

    mi primer commit

---
+++ src/p1/crew.py
@@ -0,0 +1,66 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from crewai.agents.agent_builder.base_agent import BaseAgent
+from crewai_tools import SerperDevTool  # importamos la clase
+from typing import List
+from langchain_community.llms import Ollama  # Usa Ollama como backend
+
+# If you want to run a snippet of code before or after the crew starts,
+# you can use the @before_kickoff and @after_kickoff decorators
+# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+
+# Crea una instancia de la herramienta (correcto)
+search_tool = SerperDevTool()  # ¡Los paréntesis son clave!
+
+# Configura el LLM
+ollama_llm = Ollama(model="ollama/openhermes")
+
+
+@CrewBase
+class P1:
+    """P1 crew"""
+
+    agents: List[BaseAgent]
+    tasks: List[Task]
+
+    @agent
+    def researcher(self) -> Agent:
+        return Agent(
+            config=self.agents_config["researcher"],
+            tools=[search_tool],
+            llm=ollama_llm,
+            verbose=True,
+        )
+
+    @agent
+    def reporting_analyst(self) -> Agent:
+        return Agent(
+            config=self.agents_config["reporting_analyst"],
+        )
+
+    # To learn more about structured task outputs,
+    # task dependencies, and task callbacks, check out the documentation:
+    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
+    @task
+    def research_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["research_task"],  # type: ignore[index]
+        )
+
+    @task
+    def reporting_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["reporting_task"],  # type: ignore[index]
+            output_file="report.md",
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the P1 crew"""
+
+        return Crew(
+            agents=self.agents,
+            tasks=self.tasks,
+            process=Process.sequential,
+            verbose=True,
+        )
